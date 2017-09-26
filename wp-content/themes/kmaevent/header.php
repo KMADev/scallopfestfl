1f863302c8cb6e2968d8c9d64959b12f944a751d
Jack Kerigan <support@kerigan.com>
2017-09-26 13:38:58 -0400

    Google tag

--- wp-content/themes/kmaevent/header.php
+++ wp-content/themes/kmaevent/header.php
@@ -12,6 +12,15 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-64266966-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+
+    gtag('config', 'UA-64266966-1');
+</script>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
